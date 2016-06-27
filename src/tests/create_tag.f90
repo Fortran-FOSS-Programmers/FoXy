dc644c437c5a6d1ba0a0d6476b4c15993b6dddeb
Stefano Zaghi <stefano.zaghi@gmail.com>
2016-06-27 11:23:39 +0200

    add tag function to create a tag

---
+++ src/tests/create_tag.f90
@@ -0,0 +1,35 @@
+!< FoXy test.
+program create_tag
+!-----------------------------------------------------------------------------------------------------------------------------------
+!< FoXy test.
+!-----------------------------------------------------------------------------------------------------------------------------------
+use foxy, only: tag, xml_tag
+!-----------------------------------------------------------------------------------------------------------------------------------
+
+!-----------------------------------------------------------------------------------------------------------------------------------
+implicit none
+character(len=:), allocatable :: source         !< String containing the source XML data.
+character(len=:), allocatable :: parsed         !< String containing the parsed XML data.
+type(xml_tag)                 :: a_tag          !< XML tag handler.
+logical                       :: test_passed(1) !< List of passed tests.
+!-----------------------------------------------------------------------------------------------------------------------------------
+
+!-----------------------------------------------------------------------------------------------------------------------------------
+test_passed = .false.
+
+print "(A)", 'Input XML data:'
+source = '<first x="1" y="c" z="2">lorem ipsum...</first>'
+print "(A)", source
+
+print "(A)", 'Create tag'
+a_tag = tag(tag_name='first', tag_value='lorem ipsum...', attributes_name=['x', 'y', 'z'], attributes_value=['1', 'c', '2'])
+print "(A)", 'Parsed data'
+parsed = a_tag%stringify()
+print "(A)", parsed
+test_passed(1) = trim(source)==trim(parsed)
+print "(A,L1)", 'Is parsed data correct? ', test_passed(1)
+
+print "(A,L1)", new_line('a')//'Are all tests passed? ', all(test_passed)
+stop
+!-----------------------------------------------------------------------------------------------------------------------------------
+endprogram create_tag
