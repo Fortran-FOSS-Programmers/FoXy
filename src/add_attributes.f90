f1ce44c05f22768cb7bbefa338fa3af444b9e425
Stefano Zaghi <stefano.zaghi@gmail.com>
2016-07-04 14:18:55 +0000

    Travis CI autocommit from travis build 22

--- src/add_attributes.f90
+++ src/add_attributes.f90
@@ -11,7 +11,7 @@ implicit none
 character(len=:), allocatable :: source         !< String containing the source XML data.
 character(len=:), allocatable :: parsed         !< String containing the parsed XML data.
 type(xml_tag)                 :: a_tag          !< XML tag handler.
-logical                       :: test_passed(2) !< List of passed tests.
+logical                       :: test_passed(3) !< List of passed tests.
 !-----------------------------------------------------------------------------------------------------------------------------------
 
 !-----------------------------------------------------------------------------------------------------------------------------------
@@ -35,6 +35,15 @@ print "(A)", 'created'
 parsed = a_tag%stringify()
 test_passed(2) = trim(adjustl(source))==trim(adjustl(parsed))
 print "(A,L1)", parsed//' Is correct? ', test_passed(2)
+
+call a_tag%add_attributes(attributes_stream='')
+print "(A)", 'source'
+source = '<first x="1" y="3" z="2" a="one" b="two" cc="tree">lorem ipsum...</first>'
+print "(A)", source
+print "(A)", 'created'
+parsed = a_tag%stringify()
+test_passed(3) = trim(adjustl(source))==trim(adjustl(parsed))
+print "(A,L1)", parsed//' Is correct? ', test_passed(3)
 
 print "(A,L1)", new_line('a')//'Are all tests passed? ', all(test_passed)
 stop
